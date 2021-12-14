8a2a7dce6783ed2a738d720ef3befea5367cfd45
meditative-coder <88981331+meditative-coder@users.noreply.github.com>
2021-12-14 18:32:09 +0530

    Create Range_Sum_BST.py
    Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
    
    Example 1:
    Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
    Output: 32
    Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
    
    Example 2:
    Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
    Output: 23
    Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23. 
    
    Constraints:
    The number of nodes in the tree is in the range [1, 2 * 104].
    1 <= Node.val <= 105
    1 <= low <= high <= 105
    All Node.val are unique.

---
+++ Range_Sum_BST.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int, sum_=0) -> int:        
+        if not root:
+            return 0
+        if low <= root.val <= high:
+            sum_ = sum_ + root.val
+        if root.val >= low:
+            sum_ = sum_ + self.rangeSumBST(root.left, low, high)
+        if root.val <= high:
+            sum_ =  sum_ +  self.rangeSumBST(root.right, low, high)
+        return sum_
